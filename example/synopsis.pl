fe989e193ca515a879fe2fbebefbc1ed4ae49fa0
JJ Merelo <jjmerelo@gmail.com>
2018-12-13 09:57:26 +0100

    Adds to README
    It needs pkgconf in this particular computer, so I've added it to
    README. I've also followed instructions regarding the change of the example.

--- example/synopsis.pl
+++ example/synopsis.pl
@@ -2,10 +2,9 @@
 
 use strict;
 use warnings;
+use v5.010;
 
 use Term::EditLine qw(CC_EOF);
-
-use v5.14;
 
 my $el = Term::EditLine->new('progname');
 $el->set_prompt ('# ');
@@ -15,7 +14,7 @@ $el->add_fun ('bye','desc',sub { say "\nbye"; return CC_EOF; });
 $el->parse('bind','-e');
 $el->parse('bind','^D','bye');
 
-while (defined($_ = $el->gets())) {
+while (defined( my $line = $el->gets())) {
   $el->history_enter($_);
   say $_;
 }
